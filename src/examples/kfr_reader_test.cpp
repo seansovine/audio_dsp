b1f3f800198ff300838cdfaf9d661de55d6627f6
Sean Sovine <sean.r.sovine@gmail.com>
2024-12-31 19:42:43 -0500

    Add code in kfr test program to re-write samples read from file
    This demonstrates the kfr API for reading and writing files, which
    allows us to read the samples from the channels of a wav (or other
    format) file into an array of floating-point values, regardless of
    the original sample format of the wav file. Then once we've done
    whatever processing we want on the samples, we can write them back
    out in whatever format we need.
    
    The API is clean and user-friendly. This will be very useful, both
    as a library to use and an example to learn from.

--- src/examples/kfr_reader_test.cpp
+++ src/examples/kfr_reader_test.cpp
@@ -1,3 +1,4 @@
+// A simple program to test kfr's audio file io api.
 //
 // Created by sean on 12/31/24.
 //
@@ -8,13 +9,43 @@
 
 #include <kfr/io.hpp>
 
-static const auto filename = std::string(project_root) + "/media/Low E.wav";
+static const auto inFilename = std::string(project_root) + "/media/Low E.wav";
+static const auto outFilename = std::string(project_root) + "/media/output/kfr_rewrite.wav";
 
 int main() {
-    fmt::print("Attempting to open file: {}\n", filename);
+    fmt::print("Attempting to open file: {}\n", inFilename);
 
-    auto file = kfr::open_file_for_reading(filename);
+    // Read the file with kfr.
+
+    auto file = kfr::open_file_for_reading(inFilename);
     kfr::audio_reader_wav<float> reader{file};
 
-    fmt::print("Audio file loaded successfully.\n");
+    fmt::print("Audio file loaded successfully.\n\n");
+
+    // Output some file information from the reader.
+
+    fmt::print("Sample Rate  = {}\n", reader.format().samplerate);
+    fmt::print("Channels     = {}\n", reader.format().channels);
+    fmt::print("Length       = {}\n", reader.format().length);
+    fmt::print("Duration (s) = {}\n", reader.format().length / reader.format().samplerate);
+    fmt::print("Bit depth    = {}\n", audio_sample_bit_depth(reader.format().type));
+
+    // Re-write the file with kfr to make sure we can still play it.
+
+    auto outFile = kfr::open_file_for_writing(outFilename);
+
+    // Write with same format it was originally written with.
+    auto outFormat = kfr::audio_format{reader.format().channels, reader.format().type, reader.format().samplerate};
+    kfr::audio_writer_wav<float> writer(outFile, outFormat);
+
+    // This created an empty wav file, w/ only headers.
+    // We now need to write the audio data to the file.
+
+    // Read all the file's samples into a vector of floats.
+    kfr::univector<float> data = reader.read(reader.format().length);
+
+    writer.write(data.data(), data.size());
+    writer.close();
+
+    fmt::print("\nRe-wrote audio data to file: {}\n", outFilename);
 }
